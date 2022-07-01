1050e8ea619cf705b621553eae607e9b6daddcf1
kortykotropina <kortykotropina@gmail.com>
2022-07-01 03:57:50 +0200

    Test

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,8 @@
 import DefaultTheme from "vitepress/theme";
-import VWeed from "../../../src/";
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.use(VWeed);
+    // app.use(VWeed);
   },
 };
--- docs/components/input-text.md
+++ docs/components/input-text.md
@@ -2,6 +2,4 @@
 
 `InputText` is a cool component. Here's how to use it...
 
-<template>
-  <input-text />
-</template>
+test
